6fdabdbb0a4dd50a1b74e6e6596e6db9695154b8
NourJadiri <ick62377@gmail.com>
2025-07-03 11:34:46 +0200

    Update job script with team name and correct email address; uncomment module loads

--- yourjob.sh
+++ yourjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### -- set the job Name -- 
-#BSUB -J yourjob
+#BSUB -J teamA_job
 
 ### -- specify queue -- 
 #BSUB -q c02613
@@ -22,7 +22,7 @@
 #BSUB -W 15
 
 ### -- set the email address --
-#BSUB -u s204165@student.dtu.dk
+#BSUB -u mohamed.eljadiri@insa-lyon.fr
 ### -- send notification at start --
 ##BSUB -B
 ### -- send notification at completion--
@@ -33,17 +33,19 @@
 #BSUB -o Output_%J.out 
 #BSUB -e Output_%J.err 
 
-#module load cuda/11.8 
-#module load matplotlib/3.10.0-numpy-2.2.2-python-3.10.16
-#module load numpy/2.2.2-python-3.10.16-openblas-0.3.29
-#module load pandas/2.2.3-python-3.10.16
-#module load scipy/1.15.1-python-3.10.16
+module load cuda/11.8 
+module load matplotlib/3.10.0-numpy-2.2.2-python-3.10.16
+module load numpy/2.2.2-python-3.10.16-openblas-0.3.29
+module load pandas/2.2.3-python-3.10.16
+module load scipy/1.15.1-python-3.10.16
 module load nccl/2.21.5-1-cuda-12.5 
 
 # Setup env
 source /dtu/blackhole/00/best/env_vlm_bk/bin/activate
 
 # the file to run 
+
+pip install -r requirements.txt
 python milestone_hackathon.py
 
 # run the following in terminal to submit the job
